ca2ea6236f6f35fab974bb371edc41c6502a9626
unknown <cristhian.guzman@correounivalle.edu.co>
2020-11-18 14:42:52 -0500

    add cuenta de cobro

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import 'antd/dist/antd.css';
 import '../styles/layout.css';
 import '../styles/cotizacion.css';
+import '../styles/orden_servicio.css';
 import Layout from '../components/constants/layout';
 
 function MyApp({ Component, pageProps }) {
--- pages/cuentaCobro/cuentaCobro.js
+++ pages/cuentaCobro/cuentaCobro.js
@@ -1,14 +1,30 @@
 import styles from '../../styles/Home.module.css'
-
+import ShowDate from '../../components/cotizacion/crear/showDate';
+import NumOrdenServ from '../../components/cuenta_cobro/numOrdenSer';
+import FormaPago from '../../components/cuenta_cobro/formaPago.jsx';
+import Cliente from '../../components/cuenta_cobro/cliente'
+import Total from '../../components/cuenta_cobro/total.jsx'
 function CuentaCobro() {
   return (
-    <>
-      <div>
+    <div style={{margin:'1%'}}>
+      <div className='top'>
         <h1 className={styles.title}>
           Cuenta de cobro
-       </h1>
+        </h1>
+        <ShowDate />
       </div>
-    </>)
+      <div>
+         <NumOrdenServ />        
+      </div>
+      <div style={{display: 'flex'}}>
+        <FormaPago />
+        <Cliente />
+        <Total />
+      </div>
+      
+
+    </div>
+    )
 }
 
 export default CuentaCobro;
